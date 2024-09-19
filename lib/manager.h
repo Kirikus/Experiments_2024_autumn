e8c70ebff5c97b26f6223264ba38731beb5dd642
Alexunder <skosarigin@gmail.com>
2024-09-19 23:49:40 +0300

    add Google-style format

--- lib/manager.h
+++ lib/manager.h
@@ -1,52 +1,52 @@
 #ifndef __MANAGER_H__
 #define __MANAGER_H__
 
+#include <QFile>
 #include <QList>
 #include <QObject>
-#include <QFile>
 #include <QTextStream>
 
 class VariableData {
-private:
-    QList<double> measurements;
-    QString full_name;
-    QString short_name;
-    // instrument
-    QList<double> errors;
-public:
-    VariableData(QList<double> measurements, QList<double> errors, QString full_name, QString short_name) :
-        measurements{measurements}, errors{errors}, full_name{full_name}, short_name{short_name} {}
+ private:
+  QList<double> measurements;
+  QString full_name;
+  QString short_name;
+  // instrument
+  QList<double> errors;
+
+ public:
+  VariableData(QList<double> measurements, QList<double> errors,
+               QString full_name, QString short_name)
+      : measurements{measurements},
+        errors{errors},
+        full_name{full_name},
+        short_name{short_name} {}
 };
 
 class Manager {
+ public:
+  QList<VariableData> variables;
+  QList<VariableData> calculated;
 
-public:
-    QList<VariableData> variables;
-    QList<VariableData> calculated;
+  void add_variable(VariableData var) { variables.append(var); }
 
-    void add_variable(VariableData var) {
-        variables.append(var);
-    }
+  // delete last added variable
+  void delete_variable() {
+    variables.pop_back();
+  }
 
-    void delete_variable() {
-    /* delete last added variable */
-        variables.pop_back();
-    }   
+  // clear calculated list
+  void clear_calculated() {
+    calculated = QList<VariableData>();
+    return;
+  }
 
-    void clear_calculated() {
-    /* clear calculated list */
-        calculated = QList<VariableData>();
-        return;
-    }
+  void add_calculated(VariableData var) { calculated.append(var); }
 
-    void add_calculated(VariableData var) {
-        calculated.append(var);
-    }
+  void add_measurement_row() {}
+  void remove_measurement_row() {}
 
-    void add_measurement_row() {}
-    void remove_measurement_row() {}
-
-private:
+ private:
 };
 
 #endif  // __MANAGER_H__
--- main/mainwindow.h
+++ main/mainwindow.h
@@ -1,15 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 #include <QFile>
-#include <QTextStream>
-#include <QPushButton>
-#include <QObject>
-#include <QFileDialog>
-#include <iostream>
-#include <QString>
-#include <QDataStream>
+#include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,15 +13,14 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
-public:
+ public:
   MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
-private:
+ private:
   Ui::MainWindow *ui;
 
-private slots:
+ private slots:
   void choose_file_open();
-
 };
 #endif  // MAINWINDOW_H
