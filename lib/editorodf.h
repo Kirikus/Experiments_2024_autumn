6d1cc14ae34f8092c852b02386fe9ce24c50ab75
Alexunder <skosarigin@gmail.com>
2024-09-19 23:49:40 +0300

    add Google-style format

--- lib/editorodf.h
+++ lib/editorodf.h
@@ -4,24 +4,25 @@
 #include <QList>
 
 class Block {
-public:
-    virtual void update();
-    virtual void getVisibleWidget();
-    virtual void export();
+ public:
+  virtual void update();
+  virtual void getVisibleWidget();
+  virtual void export();
 }
 
 class EditorODF {
-private:
-    QList<Block> blocks;
-public:
-    void removeBlock(Block block) {};
-    void addPlotBlock() {};
-    void addTextBlock() {};
-    void addTableBlock() {};
-    void export() {};
-    void serManager() {};
-    void moveBlockUp() {};
-    void moveBlockDown() {};
+ private:
+  QList<Block> blocks;
+
+ public:
+  void removeBlock(Block block){};
+  void addPlotBlock(){};
+  void addTextBlock(){};
+  void addTableBlock(){};
+  void export(){};
+  void serManager(){};
+  void moveBlockUp(){};
+  void moveBlockDown(){};
 }
 
 #endif  // __EDITORODF_H__
--- main/mainwindow.cc
+++ main/mainwindow.cc
@@ -1,20 +1,25 @@
 #include "mainwindow.h"
 
+#include <QDataStream>
+#include <QFileDialog>
+#include <QObject>
+#include <QPushButton>
+#include <QString>
+#include <QTextStream>
+
 #include "./ui_mainwindow.h"
 
 void MainWindow::choose_file_open() {
-  std::cout << 1 << std::endl;
-  QFile file( QFileDialog::getOpenFileName(0, "Открыть", "", "*.csv"), this );
+  QFile file(QFileDialog::getOpenFileName(0, "Открыть", "", "*.csv"), this);
 }
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
-
   ui->setupUi(this);
 
-  QPushButton* open_file_button = new QPushButton("Open file", this);
-  QObject::connect(open_file_button, SIGNAL(clicked()), SLOT(choose_file_open()));
-
+  QPushButton *open_file_button = new QPushButton("Open file", this);
+  QObject::connect(open_file_button, SIGNAL(clicked()),
+                   SLOT(choose_file_open()));
 }
 
 MainWindow::~MainWindow() { delete ui; }
