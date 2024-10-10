5149dfa005450c9d4e0ef2d948273004c2159027
Alexunder <skosarigin@gmail.com>
2024-10-10 19:07:48 +0300

    start table

--- main/mainwindow.cc
+++ main/mainwindow.cc
@@ -18,7 +18,16 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
   MeasurementModel *model = new MeasurementModel;
+  MeasurementModel *model_err = new MeasurementModel;
   Manager *manager = Manager().get_manager();
+
+  ErrorData *err = new ErrorAbsolute(0.);
+  ErrorData *err2 = new ErrorAbsolute(0.12);
+  
+  manager->add_variable(
+      VariableData({1, 2, 3}, {err2, err, err}, "Test", "tst"));
+  manager->add_variable(
+      VariableData({4, 5, 6}, {err, err2, err2}, "Test2", "tst2"));
   model->setManager(*manager);
   ui->tableView->setModel(model);
 
