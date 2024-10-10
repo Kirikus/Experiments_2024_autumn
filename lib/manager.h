bb61fb77d46f701e619151885a5c39549034ddee
Alexunder <skosarigin@gmail.com>
2024-10-10 16:50:54 +0300

    add Error classes

--- lib/manager.h
+++ lib/manager.h
@@ -3,23 +3,56 @@
 
 #include <QList>
 
+class ErrorData {
+ public:
+  double data{};
+  ErrorData(double data) : data{data} {}
+  virtual double getError(double measurement) = 0;
+};
+
+class ErrorAbsolute : public ErrorData {
+ public:
+  using ErrorData::ErrorData;
+  double getError(double measurement) override { return data; }
+};
+
+class ErrorRelative : public ErrorData {
+ public:
+  using ErrorData::ErrorData;
+  virtual double getError(double measurement) { return measurement * data; }
+};
+
+template <int StandartValue>
+class ErrorReduced : public ErrorData {
+ public:
+  using ErrorData::ErrorData;
+  virtual double getError(double measurement) {
+    return data / StandartValue * measurement;
+  }
+};
+
 class VariableData {
  public:
   QList<double> measurements{};
+  QList<ErrorData*> errors{};
 
  private:
   QString full_name{};
   QString short_name{};
   // instrument
-  QList<double> errors{};
 
  public:
-  VariableData(QList<double> measurements, QList<double> errors,
+  VariableData(QList<double> measurements, QList<ErrorData*> errors_list,
                QString full_name, QString short_name)
       : measurements{measurements},
-        errors{errors},
         full_name{full_name},
-        short_name{short_name} {}
+        short_name{short_name} {
+    ErrorData* standart_err = new ErrorAbsolute(0.);
+    errors = QList<ErrorData*>(measurements.size(), standart_err);
+    for (int i = 0; i < errors_list.size(); ++i) {
+      errors[i] = errors_list[i];
+    }
+  }
 };
 
 class Manager {
@@ -27,7 +60,7 @@ class Manager {
   QList<VariableData> variables{};
   QList<VariableData> calculated{};
 
-  Manager* get_manager();
+  static Manager* get_manager();
 
   void add_variable(VariableData var) { variables.append(var); }
 
@@ -42,11 +75,14 @@ class Manager {
   void add_measurement_row() {
     for (int i = 0; i < variables.size(); ++i) {
       variables[i].measurements.push_back(.0);
+      ErrorData* standart_err = new ErrorAbsolute(0.);
+      variables[i].errors.push_back(standart_err);
     }
   }
   void remove_measurement_row() {
     for (int i = 0; i < variables.size(); ++i) {
       variables[i].measurements.pop_back();
+      variables[i].errors.pop_back();
     }
   }
 
