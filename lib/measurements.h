870bcd932792a75c94699804885d5053ede83f8b
Alexunder <skosarigin@gmail.com>
2024-10-17 14:15:26 +0300

    add ErrorModel

--- lib/measurements.h
+++ lib/measurements.h
@@ -8,8 +8,16 @@
 #include "manager.h"
 
 class MeasurementModel : public QAbstractTableModel {
- private:
+ protected:
   Manager p_manager;
+
+ public slots:
+
+  void recalculate_data(const QModelIndex& index) {
+    std::cout << index.row() << " - " << index.column() << std::endl;
+    std::cout << p_manager.variables[index.row()].errors_local[index.column()]
+              << std::endl;
+  }
 
  public:
   virtual QVariant headerData(int section, Qt::Orientation orientation,
@@ -86,6 +94,95 @@ class MeasurementModel : public QAbstractTableModel {
   }
 
   virtual void setManager(Manager& manager) { p_manager = manager; }
+};
+
+class ErrorModel : public MeasurementModel {
+ public:
+ private:
+  virtual QVariant data(const QModelIndex& index,
+                        int role = Qt::DisplayRole) const {
+    int row = index.row();
+    int col = index.column();
+
+    ErrorData* error;
+    if (!p_manager.variables[index.row()].errors_local[index.column()])
+      error = p_manager.variables[index.row()].error_global;
+    else {
+      error = p_manager.variables[index.row()].errors_local[index.column()];
+    }
+
+    if (error->getError(1) !=
+        error->getError(2)) {  // check: *error != ErrorAbsolute
+      return QString("%1%").arg(error->data * 100);
+    }
+
+    return error->data;
+  }
+
+  virtual bool setData(const QModelIndex& index, const QVariant& value,
+                       int role = Qt::EditRole) {
+    if (role == Qt::EditRole) {
+      QString input = value.toString();
+      ErrorData* new_error;
+      double data;
+      bool* success;
+      if (input.right(1) == '%') {
+        input.chop(1);
+        input.toDouble(success);
+        if (input.isEmpty() || !*success) {
+          std::cout << input.toStdString() << 1 << std::endl;
+          return false;
+        }
+        data = input.toDouble();
+        if (data < 0) {
+          std::cout << 4 << std::endl;
+          return false;
+        }
+
+        new_error = new ErrorRelative(data / 100);
+      } else {
+        input.toDouble(success);
+        if (input.isEmpty() || !*success) {
+          std::cout << 2 << std::endl;
+          return false;
+        }
+        data = input.toDouble();
+        if (data < 0) {
+          std::cout << 3 << std::endl;
+          return false;
+        }
+
+        new_error = new ErrorAbsolute(data);
+      }
+
+      ErrorData* error;
+      if (!p_manager.variables[index.row()].errors_local[index.column()]) {
+        error = p_manager.variables[index.row()].error_global;
+      } else {
+        error = p_manager.variables[index.row()].errors_local[index.column()];
+      }
+
+      if (error->data != data) {
+        delete p_manager.variables[index.row()].errors_local[index.column()];
+        p_manager.variables[index.row()].errors_local[index.column()] =
+            new_error;
+      }
+
+      std::cout
+          << p_manager.variables[index.row()].errors_local[index.column()]->data
+          << " ee"
+          << "  " << index.row() << " == " << index.column() << std::endl;
+      emit dataChanged(index, index, QList({role}));
+      std::cout
+          << p_manager.variables[index.row()].errors_local[index.column()]->data
+          << " fgf"
+          << "  " << index.row() << " == " << index.column() << std::endl;
+
+      std::cout << 1 << std::endl;
+      return true;
+    }
+    return false;
+  }
 };
 
 class MeasurementView : public QTableView {};
