5dbfe5eb506dab3cffe6e9272158e991b470caec
Alexunder <skosarigin@gmail.com>
2024-10-20 17:39:41 +0300

    delegating translation to QString for output for ErrorData

--- lib/errors_to_variable.h
+++ lib/errors_to_variable.h
@@ -6,18 +6,21 @@ class ErrorData {
   double data{};
   ErrorData(double data) : data{data} {}
   virtual double getError(double measurement) = 0;
+  virtual operator QString() const = 0;
 };
 
 class ErrorAbsolute : public ErrorData {
  public:
   using ErrorData::ErrorData;
   double getError(double measurement) override { return data; }
+  virtual operator QString() const { return QString::number(data); }
 };
 
 class ErrorRelative : public ErrorData {
  public:
   using ErrorData::ErrorData;
   virtual double getError(double measurement) { return measurement * data; }
+  virtual operator QString() const { return QString::number(data * 100) + "%"; }
 };
 
 #endif
