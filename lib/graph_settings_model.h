f35f03b2518583fa3cf00b9943ef35e6e52a434a
Alexunder <skosarigin@gmail.com>
2024-11-15 06:44:46 +0300

    add enum, use new delegates

--- lib/graph_settings_model.h
+++ lib/graph_settings_model.h
@@ -1,36 +1,55 @@
 #ifndef __GRAPH_SETTINGS_MODEL_H__
 #define __GRAPH_SETTINGS_MODEL_H__
 
+#include <QCheckBox>
 #include <QTableWidget>
 
+#include "delegates.h"
 #include "manager.h"
 
 class SettingsModel : public QTableWidget {
  public:
+  enum Column {
+    Is_Active = 0,  // Column for switching graph on/off
+    Style,          // Column for line type
+    Color,          // Column for graph color
+    Scatter,        // Column for scatters type at graph
+    Scatter_Size,   // Column for size of scatters
+    Line_Size       // Column for line size
+  };
+  QList<QString> heading{"Is Active", "Style",        "Color",
+                         "Scatter",   "Scatter Size", "Line Size"};
+
   SettingsModel(QWidget* parent = nullptr) : QTableWidget(parent) {
     setRowCount(Manager::get_manager().variables.size());
-    setColumnCount(3);
-    setItemDelegateForColumn(1, new LineStyleDelegate());
-    setItemDelegateForColumn(2, new ColorDelegate());
+    setColumnCount(heading.size());
+
+    setItemDelegateForColumn(Is_Active, new CheckBoxDelegate());
+    setItemDelegateForColumn(Style, new LineStyleDelegate());
+    setItemDelegateForColumn(Color, new ColorDelegate());
+    setItemDelegateForColumn(Scatter, new ScatterStyleDelegate());
 
     int rows_size = rowCount();
     for (int i = 0; i < rows_size; ++i) {
-      QTableWidgetItem* item0 = new QTableWidgetItem("1");
-      QTableWidgetItem* itemNone = new QTableWidgetItem("None");
+      QTableWidgetItem* item0 = new QTableWidgetItem("0");
+      QTableWidgetItem* itemNone = new QTableWidgetItem("Line");
       QTableWidgetItem* itemColor = new QTableWidgetItem("");
+      QTableWidgetItem* itemScatter = new QTableWidgetItem("None");
+      QTableWidgetItem* itemScatterSize = new QTableWidgetItem("10");
+      QTableWidgetItem* itemLineSize = new QTableWidgetItem("2");
 
-      setItem(i, 0, item0);
-      setItem(i, 1, itemNone);
-      setItem(i, 2, itemColor);
+      setItem(i, Is_Active, item0);
+      setItem(i, Style, itemNone);
+      setItem(i, Color, itemColor);
+      setItem(i, Scatter, itemScatter);
+      setItem(i, Scatter_Size, itemScatterSize);
+      setItem(i, Line_Size, itemLineSize);
+
+      QRgb rgb = qRgba(rand() % 255, rand() % 255, rand() % 255, 255);
+      item(i, Color)->setBackground(QBrush(QColor(rgb)));
     }
 
-    QTableWidgetItem* item0 = new QTableWidgetItem("Is Active");
-    QTableWidgetItem* item1 = new QTableWidgetItem("Style");
-    QTableWidgetItem* item2 = new QTableWidgetItem("Color");
-
-    setHorizontalHeaderItem(0, item0);
-    setHorizontalHeaderItem(1, item1);
-    setHorizontalHeaderItem(2, item2);
+    setHorizontalHeaderLabels(heading);
   }
 };
 
