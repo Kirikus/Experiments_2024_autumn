e7ffe037497b2ce314dfa4564b8e12869517a4ea
Alexunder <skosarigin@gmail.com>
2024-11-18 01:43:32 +0300

    add fillDefaultValues slot

--- lib/graph_settings_model.h
+++ lib/graph_settings_model.h
@@ -21,35 +21,32 @@ class SettingsModel : public QTableWidget {
                          "Scatter",   "Scatter Size", "Line Size"};
 
   SettingsModel(QWidget* parent = nullptr) : QTableWidget(parent) {
-    setRowCount(Manager::get_manager().variables.size());
+    connect(model(), &QAbstractItemModel::rowsInserted, this,
+            &SettingsModel::fillDefaultValues);
+
     setColumnCount(heading.size());
+    setRowCount(Manager::get_manager().variables.size());
 
     setItemDelegateForColumn(Is_Active, new CheckBoxDelegate());
     setItemDelegateForColumn(Style, new LineStyleDelegate());
     setItemDelegateForColumn(Color, new ColorDelegate());
     setItemDelegateForColumn(Scatter, new ScatterStyleDelegate());
 
-    int rows_size = rowCount();
-    for (int i = 0; i < rows_size; ++i) {
-      QTableWidgetItem* itemActive = new QTableWidgetItem("1");
-      QTableWidgetItem* itemNone = new QTableWidgetItem("Line");
-      QTableWidgetItem* itemColor = new QTableWidgetItem("");
-      QTableWidgetItem* itemScatter = new QTableWidgetItem("None");
-      QTableWidgetItem* itemScatterSize = new QTableWidgetItem("10");
-      QTableWidgetItem* itemLineSize = new QTableWidgetItem("2");
-
-      setItem(i, Is_Active, itemActive);
-      setItem(i, Style, itemNone);
-      setItem(i, Color, itemColor);
-      setItem(i, Scatter, itemScatter);
-      setItem(i, Scatter_Size, itemScatterSize);
-      setItem(i, Line_Size, itemLineSize);
-
-      QRgb rgb = qRgba(rand() % 255, rand() % 255, rand() % 255, 255);
-      item(i, Color)->setBackground(QBrush(QColor(rgb)));
-    }
-
     setHorizontalHeaderLabels(heading);
+  }
+
+ private slots:
+  void fillDefaultValues(const QModelIndex& parent, int start, int end) {
+    for (int row = start; row <= end; row++) {
+      setItem(row, Column::Is_Active, new QTableWidgetItem("1"));
+      setItem(row, Column::Style, new QTableWidgetItem("Line"));
+      setItem(row, Column::Color, new QTableWidgetItem(""));
+      QRgb rgb = qRgba(rand() % 255, rand() % 255, rand() % 255, 255);
+      item(row, Column::Color)->setBackground(QBrush(QColor(rgb)));
+      setItem(row, Column::Scatter, new QTableWidgetItem("None"));
+      setItem(row, Column::Scatter_Size, new QTableWidgetItem("10"));
+      setItem(row, Column::Line_Size, new QTableWidgetItem("2"));
+    }
   }
 };
 
