7812f275b97cb3d9137e69374a88e3ebbc181f3a
Salnikova-Lida <salnikova.lm@phystech.edu>
2024-11-20 17:15:27 +0300

    return title page

--- main/mainwindow.cc
+++ main/mainwindow.cc
@@ -10,6 +10,7 @@
 #include <QVector>
 
 #include "../lib/error_model.h"
+#include "../lib/titles_model.h"
 #include "../lib/measurement_model.h"
 #include "../lib/plots.h"
 #include "./ui_mainwindow.h"
@@ -31,8 +32,10 @@ MainWindow::MainWindow(QWidget *parent)
   Manager &manager = Manager().get_manager();
   MeasurementModel *model_measurements = new MeasurementModel;
   ErrorModel *model_err = new ErrorModel;
+  TitleModel *model_titles = new TitleModel;
   ErrorData *err = new ErrorAbsolute(0.);
   ErrorData *err2 = new ErrorRelative(0.12);
+
 
   manager.add_variable(VariableData({1, 3.5, -4}, err, "Test", "tst"));
   manager.add_variable(VariableData({4, 25, 26}, err2, "Test2", "tst2"));
@@ -42,6 +45,7 @@ MainWindow::MainWindow(QWidget *parent)
 
   ui->tableData->setModel(model_measurements);
   ui->tableErrors->setModel(model_err);
+  ui->tableTitles->setModel(model_titles);
   ui->graphics->addTab(plot, "Scatter");
 
   connect(ui->buttonGraph, &QPushButton::clicked, this, &MainWindow::create_dialog);
