522ae2073fbff219d3c82444e0800000353d9889
Alexunder <skosarigin@gmail.com>
2024-11-26 20:41:09 +0300

    separate into .h and .cpp

--- lib/graph_settings_model.h
+++ lib/graph_settings_model.h
@@ -1,11 +1,7 @@
 #ifndef __GRAPH_SETTINGS_MODEL_H__
 #define __GRAPH_SETTINGS_MODEL_H__
 
-#include <QCheckBox>
 #include <QTableWidget>
-
-#include "delegates.h"
-#include "manager.h"
 
 class SettingsModel : public QTableWidget {
  public:
@@ -17,37 +13,12 @@ class SettingsModel : public QTableWidget {
     Scatter_Size,   // Column for size of scatters
     Line_Size       // Column for line size
   };
-  QList<QString> heading{"Is Active", "Style",        "Color",
-                         "Scatter",   "Scatter Size", "Line Size"};
+  static QList<QString> heading;
 
-  SettingsModel(QWidget* parent = nullptr) : QTableWidget(parent) {
-    connect(model(), &QAbstractItemModel::rowsInserted, this,
-            &SettingsModel::fillDefaultValues);
-
-    setColumnCount(heading.size());
-    setRowCount(Manager::get_manager().variables.size());
-
-    setItemDelegateForColumn(Is_Active, new CheckBoxDelegate());
-    setItemDelegateForColumn(Style, new LineStyleDelegate());
-    setItemDelegateForColumn(Color, new ColorDelegate());
-    setItemDelegateForColumn(Scatter, new ScatterStyleDelegate());
-
-    setHorizontalHeaderLabels(heading);
-  }
+  SettingsModel(QWidget* parent);
 
  private slots:
-  void fillDefaultValues(const QModelIndex& parent, int start, int end) {
-    for (int row = start; row <= end; row++) {
-      setItem(row, Column::Is_Active, new QTableWidgetItem("1"));
-      setItem(row, Column::Style, new QTableWidgetItem("Line"));
-      setItem(row, Column::Color, new QTableWidgetItem(""));
-      QRgb rgb = qRgba(rand() % 255, rand() % 255, rand() % 255, 255);
-      item(row, Column::Color)->setBackground(QBrush(QColor(rgb)));
-      setItem(row, Column::Scatter, new QTableWidgetItem("None"));
-      setItem(row, Column::Scatter_Size, new QTableWidgetItem("10"));
-      setItem(row, Column::Line_Size, new QTableWidgetItem("3"));
-    }
-  }
+  void fillDefaultValues(const QModelIndex& parent, int start, int end);
 };
 
 #endif
