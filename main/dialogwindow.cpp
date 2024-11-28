2c6b84922c69753d170ad0ba4602bd8fe28ec86a
Alexunder <skosarigin@gmail.com>
2024-11-28 19:37:38 +0300

    and generated new tab's name

--- main/dialogwindow.cpp
+++ main/dialogwindow.cpp
@@ -12,6 +12,8 @@ void DialogWindow::create_graph() {
   QString chosen_type =
       ui->comboBox->currentData(Qt::DisplayRole).value<QString>();
   int t = plot_type_map[chosen_type];
+  QString new_name =
+      chosen_type + "_" + QString::number(target_tab_widget->count());
   switch (t) {
     case OneAxisPlotType: {
       int index = target_tab_widget->addTab(new OneAxisPlot(), "NewTab");
